14123540a2c79ade984d4448d76f371870fac406
patel0ankur <39783407+patel0ankur@users.noreply.github.com>
2018-06-07 00:24:20 -0500

    create ebs vol

---
+++ create_ebs_vol.py
@@ -0,0 +1,13 @@
+import boto3
+
+ec2 = boto3.client('ec2')
+
+#Create Volume
+vol = ec2.create_volume(AvailabilityZone='us-east-1a', Size=10, VolumeType='standard')
+#print(vol)
+
+#Tag the volume
+ec2.create_tags(Resources=[vol['VolumeId']], Tags=[{'Key':'name', 'Value':'testvolume'}])
+
+#List Created volume id
+print("Created vol ID: " + vol['VolumeId'])
