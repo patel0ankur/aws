5d41fa1a70f9b965f0c209436be348323ab5b10d
Ankur Patel <39783407+patel0ankur@users.noreply.github.com>
2018-06-13 22:59:06 -0500

    Add Comment

--- create_iam_user.py
+++ create_iam_user.py
@@ -3,10 +3,14 @@ import boto3
 # Create IAM client
 iam = boto3.client('iam')
 
+#Enter username 
 username = input('Enter UserName: ')
 
+#Create Username
 response = iam.create_user(
     UserName=username
 )
+
+#Print Created UserName
 print("User " + response['User']['UserName'] + " created")
 
