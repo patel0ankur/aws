d8d03b2a0bb1307a4a3864ff27772ba1bdf6f23b
Ankur Patel <39783407+patel0ankur@users.noreply.github.com>
2018-06-13 23:00:27 -0500

    Add Comment

--- list_iam_users.py
+++ list_iam_users.py
@@ -4,10 +4,14 @@ iam = boto3.client('iam')
 
 def user_details():
     for user in iam.list_users()['Users']:
+	
+	#Print IAM Users 
         print("UserName: " + user['UserName'])
 
         Groups = iam.list_groups_for_user(UserName=user['UserName'])
         for groupname in Groups['Groups']:
+		
+		#print GroupName assign to Users
             print("GroupName: " + groupname['GroupName'])
 
         print("----------------------------")
