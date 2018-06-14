dcc22af9d0956250552a25aa19d4579949334f48
Ankur Patel <39783407+patel0ankur@users.noreply.github.com>
2018-06-13 21:49:37 -0500

    describe instances
    Describe instances as per tag name

---
+++ describe_instances.py
@@ -0,0 +1,12 @@
+import boto3
+
+ec2 = boto3.client('ec2')
+
+print("\n =================================  \n")
+response = ec2.describe_instances(Filters=[{'Name': 'tag:ENV', 'Values': ['DEV']}])
+for reservation in response['Reservations']:
+    for instances in reservation['Instances']:
+        print("Instance IP address: " + instances['PrivateIpAddress'])
+        print("Instance Type: " + instances['InstanceType'])
+        print("Instance State: " + instances['State']['Name'])
+        print("\n ================================ \n")
