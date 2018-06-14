9ae7a744852e9b2cb64a54d17042ee9433eb4601
Ankur Patel <39783407+patel0ankur@users.noreply.github.com>
2018-06-13 23:01:19 -0500

    Add Comment

--- modify_volume.py
+++ modify_volume.py
@@ -2,14 +2,19 @@ import boto3
 
 ec2 = boto3.client("ec2")
 
+#Read Instances ID from inst.txt file
 nodes = open("instances.txt").read().replace("\n", "").split(',')
 
+#List volumes of attached instace id
 response = ec2.describe_volumes(Filters = [{'Name': 'attachment.instance-id','Values': nodes}])
 
+#Print Volume Id of Intances 
 for vol in response["Volumes"]:
     for attach in vol['Attachments']:
         vol = [(attach['VolumeId'])]
+		#print(vol)
 
+#Increase volume size of Volumes
     for vol_id in vol:
         response = ec2.modify_volume(VolumeId=vol_id, Size=30)
         print(response)
