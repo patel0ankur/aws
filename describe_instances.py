ca5857436b1628f0b286db4a1bc788d38d8ef79b
Ankur Patel <39783407+patel0ankur@users.noreply.github.com>
2018-06-13 23:00:19 -0500

    Describe Instances
    Describe Instances

--- describe_instances.py
+++ describe_instances.py
@@ -3,6 +3,8 @@ import boto3
 ec2 = boto3.client('ec2')
 
 print("\n =================================  \n")
+
+#Filter instaces using Tag Dev env
 response = ec2.describe_instances(Filters=[{'Name': 'tag:ENV', 'Values': ['DEV']}])
 for reservation in response['Reservations']:
     for instances in reservation['Instances']:
