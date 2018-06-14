10a72abfabda81625c093555628cd35f1be6487b
Ankur Patel <39783407+patel0ankur@users.noreply.github.com>
2018-06-13 22:58:47 -0500

    Add Comment

--- add_IAMuser_group.py
+++ add_IAMuser_group.py
@@ -2,16 +2,20 @@ import boto3
 
 iam = boto3.client('iam')
 
+#Enter Name of User and Group
 username = input('Enter UserName: ')
 groupname = input('Enter GroupName: ')
 
+# Add user to group
 response = iam.add_user_to_group(
     GroupName=groupname,
     UserName=username
 )
 
+# If response is 200, print Username added to group
 if (response['ResponseMetadata']['HTTPStatusCode']) == 200:
     print("UserName " + username + " added to group " + groupname)
 
+# Print error
 else:
     print("Not Added")
