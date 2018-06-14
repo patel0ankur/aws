53a37a840c0335566444da4b225163fb036b0f68
Ankur Patel <39783407+patel0ankur@users.noreply.github.com>
2018-06-13 22:58:58 -0500

    Add Comment

--- attached_policy_IAMgroup.py
+++ attached_policy_IAMgroup.py
@@ -2,18 +2,23 @@ import boto3
 
 iam = boto3.client('iam')
 
+#Enter name of group and ARN of policy to attach 
 groupname = input('Enter GroupName: ')
 policyARN = input('Enter Policy ARN: ')
 
+#Split / from Policy ARN
 arn = (policyARN.split('/'))
 
+#Attach Policy to Group
 response = iam.attach_group_policy(
     GroupName=groupname,
     PolicyArn=policyARN
 )
 
+# if Response is 200, Print attaced policy to group
 if (response['ResponseMetadata']['HTTPStatusCode']) == 200:
     print("Policy " + arn[1] + " attached to group " + groupname)
 
+# Else print error
 else:
     print("Not Attached")
