f170cc09b80ad0d72d2c8322d40852f1cf046a53
Ankur Patel <39783407+patel0ankur@users.noreply.github.com>
2018-06-13 22:59:27 -0500

    Add Comment

--- create_snapshots.py
+++ create_snapshots.py
@@ -3,27 +3,23 @@ from botocore.exceptions import ClientError
 
 ec2_client = boto3.client("ec2")
 
+#Filter volumes using Tags Dev Env
 try:
-   ec2_volumes = ec2_client.describe_volumes(Filters = [
-           {
-               'Name': "tag-key",
-               'Values': ["xxxxx"]
-           },
-           {
-               'Name': "tag-value",
-               'Values': ['xxxxx']
-           }
-       ])
+   volumes = ec2_client.describe_volumes(Filters=[{'Name': 'tag:ENV','Values': ['DEV']}])
 
-   for volume in ec2_volumes.get('Volumes'):
-       volume_id = volume.get('VolumeId')
+   for vol in volumes.get('Volumes'):
+       volume_id = vol.get('VolumeId')
 
        try:
+
+           # Create Snapshots of Volumes and put description
            snapshot = ec2_client.create_snapshot(VolumeId=volume_id, Description="Automatic Backup")
            #print("Snapshot ID " + snapshot['SnapshotId'])
 
+#Error handling if snapshots fails
        except ClientError as err:
            print("Error in snapshot" + err)
-
+           
+#Error handling if Describe volumes fails
 except ClientError as err:
    print("Error in volume describe" + err)
